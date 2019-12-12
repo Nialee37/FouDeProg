71c03b85ab08596f571a55d1219e64d2bdeed75e
Skkay <skkay.thomas@gmail.com>
2019-12-12 17:15:06 +0100

    Commencement classe Utilisateur

---
+++ FunEnBulles/FunEnBulles/Gestion_Utilisateur.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace FunEnBulles
+{
+    /// <summary>
+    /// Logique d'interaction pour Gestion_Utilisateur.xaml
+    /// </summary>
+    public partial class Gestion_Utilisateur : Window
+    {
+        private MySqlConnection connection;
+        public Gestion_Utilisateur()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void cmd_ajouter(object sender, RoutedEventArgs e)
+        {
+            // Création de l'objet Command et association à la procédure stockée
+            MySqlCommand cmd_ajout_utilisateur = new MySqlCommand();
+            cmd_ajout_utilisateur.CommandText = "utilisateur_ajouter";
+            cmd_ajout_utilisateur.CommandType = CommandType.StoredProcedure;
+            cmd_ajout_utilisateur.Connection = connection;
+
+            // Paramètre d'entrée
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("matricule", MySqlDbType.String));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("nom", MySqlDbType.String));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("prenom", MySqlDbType.String));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("ville", MySqlDbType.String));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("code_postal", MySqlDbType.String));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("adresse", MySqlDbType.String));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("telephone", MySqlDbType.String));
+            // cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("droit_administration", MySqlDbType.String));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("date_naissance", MySqlDbType.DateTime));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("email", MySqlDbType.DateTime));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("pseudonyme", MySqlDbType.DateTime));
+            cmd_ajout_utilisateur.Parameters.Add(new MySqlParameter("mot_de_passe", MySqlDbType.DateTime));
+
+            // Affectation des valeurs
+            try
+            {
+                cmd_ajout_utilisateur.Parameters["matricule"].Value = txt_matricule.Text;
+                cmd_ajout_utilisateur.Parameters["nom"].Value = txt_nom.Text;
+                cmd_ajout_utilisateur.Parameters["prenom"].Value = txt_prenom.Text;
+                cmd_ajout_utilisateur.Parameters["ville"].Value = txt_ville.Text;
+                cmd_ajout_utilisateur.Parameters["code_postal"].Value = txt_code_postal.Text;
+                cmd_ajout_utilisateur.Parameters["adresse"].Value = txt_adresse.Text;
+                cmd_ajout_utilisateur.Parameters["telephone"].Value = txt_telephone.Text;
+                // cmd_ajout_utilisateur.Parameters["droit_administration"].Value = txt;
+                cmd_ajout_utilisateur.Parameters["date_naissance"].Value = ;
+                cmd_ajout_utilisateur.Parameters["email"].Value = ;
+                cmd_ajout_utilisateur.Parameters["pseudonyme"].Value = ;
+                cmd_ajout_utilisateur.Parameters["mot_de_passe"].Value = ;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private bool ConnexionBdd()
+        {
+            connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["connectBdd"].ConnectionString);
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+            }
+        }
+
+        private void fermerConnexion()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ FunEnBulles/lib_funEnBulles/Utilisateur.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lib_funEnBulles
+{
+    public class Utilisateur
+    {
+        private string _Matricule;
+        private string _Nom;
+        private string _Prenom;
+        private string _Ville;
+        private string _CodePostal;
+        private string _Adresse;
+        private string _Telephone;
+        private string _Email;
+        private string _Pseudo;
+        private string _Mot_de_passe;
+        private string _Droit;
+        private DateTime _DateNaissance;
+
+        public string Matricule { get => _Matricule; set => _Matricule = value; }
+        public string Nom { get => _Nom; set => _Nom = value; }
+        public string Prenom { get => _Prenom; set => _Prenom = value; }
+        public string Ville { get => _Ville; set => _Ville = value; }
+        public string CodePostal { get => _CodePostal; set => _CodePostal = value; }
+        public string Adresse { get => _Adresse; set => _Adresse = value; }
+        public string Telephone { get => _Telephone; set => _Telephone = value; }
+        public string Email { get => _Email; set => _Email = value; }
+        public string Pseudo { get => _Pseudo; set => _Pseudo = value; }
+        public string Mot_de_passe { get => _Mot_de_passe; set => _Mot_de_passe = value; }
+        public string Droit { get => _Droit; set => _Droit = value; }
+        public DateTime DateNaissance { get => _DateNaissance; set => _DateNaissance = value; }
+    }
+}
